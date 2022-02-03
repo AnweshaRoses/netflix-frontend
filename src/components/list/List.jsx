393e0c732ed0833f2e89606272320e94803210b1
Anwesha Mishra <anwesharoses@gmail.com>
2022-02-04 03:37:31 +0800

    Movie Slider Finished + added icons on hover of movies

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,18 +1,24 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import ListItem from '../listItem/ListItem';
 import "./list.scss"
 
 export default function List() {
 
+    const [isMoved, setIsMoved]=useState(false)
+    const [slideNumber, setSlideNumber]=useState(0)
+
     const listRef=useRef()
 
     const handleClick=(direction)=>{
+        setIsMoved(true)
         let distance=listRef.current.getBoundingClientRect().x-50
-        if(direction === "left"){
+        if(direction === "left" && slideNumber>0){
+            setSlideNumber(slideNumber-1)
             listRef.current.style.transform=`translateX(${230+distance}px)`
         }
-        if(direction === "right"){
+        if(direction === "right" && slideNumber < 6){
+            setSlideNumber(slideNumber+1)
             listRef.current.style.transform=`translateX(${-230+distance}px)`
         }
 
@@ -21,8 +27,10 @@ export default function List() {
     <div className='list'>
         <span className="listTitle">Continue to Watch</span>
         <div className="wrapper">
-            <ArrowBackIosOutlined className='sliderArrow left' onClick={()=>handleClick("left")} />
+            <ArrowBackIosOutlined className='sliderArrow left' onClick={()=>handleClick("left")} style={{display : !isMoved && "none"}} />
             <div className="container" ref={listRef}>
+                <ListItem/>
+                <ListItem/>
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
   <div className='home'>
       <Navbar/>
-      <Featured />
+      <Featured type="series"/>
       <List/>
       <List/>
       <List/>
