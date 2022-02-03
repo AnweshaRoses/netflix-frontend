85928f7a15a7e3f852ee79e12403c0d5298c8961
Anwesha Mishra <anwesharoses@gmail.com>
2022-02-04 02:12:09 +0800

    added a genre dropdown menu for movies and tv-series

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,45 @@
+import { InfoOutlined, PlayArrow } from '@material-ui/icons';
+import React from 'react';
+import "./featured.scss"
+export default function Featured({type}) {
+  return(
+    <div className='Featured'>
+        {type && (
+            <div className="category">
+                <span>{type==="movie" ? "Movies": "Series"}</span>
+                <select name="genre" id="genre">
+                    <option >Genre</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="crime">Crime</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="historical">Historical</option>
+                    <option value="horror">Horror</option>
+                    <option value="romance">Romance</option>
+                    <option value="sci-fi">Sci-fi</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="western">Western</option>
+                    <option value="animation">Animation</option>
+                    <option value="drama">Drama</option>
+                    <option value="documentary">Documentary</option>
+                </select>
+            </div>
+        )}
+        <img src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="" />
+        <div className="info">
+            <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
+            <span className="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem quaerat corrupti similique eaque ex sed! Ea recusandae esse doloremque voluptas incidunt explicabo atque optio alias unde, quasi accusamus ad voluptatum repudiandae similique deleniti molestias.</span>
+            <div className="buttons">
+                <button className="play">
+                    <PlayArrow/>
+                    <span>Play</span>
+                </button>
+                <button className="more">
+                    <InfoOutlined/>
+                    <span>Info</span>
+                </button>
+            </div>
+        </div>
+    </div>
+  ) 
+}
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Featured from '../components/featured/Featured';
 import Navbar from '../components/navbar/Navbar';
 import "./home.scss"
 
@@ -6,7 +7,7 @@ const Home = () => {
   return (
   <div className='home'>
       <Navbar/>
-      <img width="100%" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="" />
+      <Featured />
   </div>
   )
 };
